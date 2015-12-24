1245a64d881662028a1cf6b4421404285f23bebc
James Cool <jeffreysbrother@gmail.com>
2015-12-23 19:12:36 -0800

    function override

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ var automobile = {
   engine: "V8",
   topSpeed: 187,
   run: function(){
-    console.log("My name is Ford");
+    console.log("I'm a brand new Ford; I started up just fine.");
   }
 };
 
-// an example of delegation
+// an example of delegation (RECOMMENDED)
 var auto2 = Object.create(automobile);
 auto2.engine = "V6";
 auto2.name = "Camry";
+auto2.pastOwners = ["Karyn Cool", "Charles Frazer"];
+auto2.currentOwner = "James Cool";
+auto2.run = function() {
+  console.log("I'm having a hard time starting!!");
+}
 
-//constructor function
+//constructor function (not recommended)
 function Person(firstname, lastname) {
   this.firstname = firstname;
   this.lastname = lastname;
